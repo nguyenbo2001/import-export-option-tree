100acc5353791b2f5366329bb2e7ba0e87ec8a66
Bo Nguyen <nguyenbo2001@gmail.com>
2020-02-18 12:29:46 +0700

    Initialize commit

---
+++ import-export-option-tree.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Import Export Option Tree
+ * Description: Import and Export theme setting on Option Tree
+ * Version: 1.0
+ * Author: Bonguyen
+ * Plugin URI: https://github.com/nguyenbo2001/import-export-option-tree.git
+ * Author URI: https://nguyenbo2001.github.io
+ * Lisence: MIT
+ */
